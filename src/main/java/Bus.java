d225c742f3d93fab15908a3625cfa98c2f6bfcbd
BrockBaron <Nslekkas@gmail.com>
2021-05-18 15:51:29 +0100

    some tests completed

--- src/main/java/Bus.java
+++ src/main/java/Bus.java
@@ -1,4 +1,22 @@
+import java.util.ArrayList;
+
 public class Bus {
 
-    
+    private String destination;
+    private Integer capacity;
+    private ArrayList<Person> passengers;
+
+    public Bus(String destination, int capacity){
+        this.destination = destination;
+        this.capacity = 5;
+        this.passengers = new ArrayList<>();
+    }
+
+    public int getPassengerCount() {
+        return this.passengers.size();
+    }
+
+    public void addPassenger(Person person) {
+        passengers.add(person);
+    }
 }
--- src/test/java/BusTest.java
+++ src/test/java/BusTest.java
@@ -1,2 +1,40 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class BusTest {
+
+    private Bus bus;
+    private Person person;
+
+
+    @Before
+    public void before(){
+        bus = new Bus("Edinburgh", 0);
+        person = new Person();
+    }
+
+    @Test
+    public void getPassengerOnBus() {
+        assertEquals(0, bus.getPassengerCount());
+    }
+
+    @Test
+    public void canGetAddPassengerToBus() {
+        bus.addPassenger(person);
+        assertEquals(1, bus.getPassengerCount());
+    }
+
+//    @Test
+    public void addPassengerToBusIfThereIsCapacity() {
+        bus.addPassenger(person);
+        bus.addPassenger(person);
+        bus.addPassenger(person);
+        bus.addPassenger(person);
+        bus.addPassenger(person);
+        bus.addPassenger(person);
+        assertEquals(6, bus.getPassengerCount());
+
+    }
 }
